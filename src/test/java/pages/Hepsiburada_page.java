7e6f9d4f79628d3263448d26e5f00a171f86597d
ahmetdoruk <ahmet_eagle1903@hotmail.com>
2022-10-26 01:56:35 +0300

    projeye baslandi

---
+++ src/test/java/pages/Hepsiburada_page.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Hepsiburada_page extends BasePage{
+
+
+    @FindBy(id = "onetrust-accept-btn-handler")
+    public WebElement cerezKullanımıKabul;
+
+    @FindBy (xpath = "(//span[@title='Giriş Yap'])")
+    public WebElement GirisYap;
+
+    @FindBy (id = "login")
+    public WebElement GirisYapButton;
+
+    @FindBy (id = "txtUserName")
+    public WebElement email;
+
+    @FindBy(id = "btnLogin")
+    public WebElement loginButton;
+}
---
+++ src/test/java/pages/StepDefinition.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.BrowserUtils;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+import static org.openqa.selenium.Keys.ENTER;
+
+public class StepDefinition {
+
+    Hepsiburada_page hepsiburada_page = new Hepsiburada_page ();
+
+    @BeforeMethod
+    public void setupMethod() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("hepsiburada.url"));
+        hepsiburada_page = new Hepsiburada_page();
+    }
+
+    @Test
+    public void test() {
+
+
+        hepsiburada_page.cerezKullanımıKabul.click();
+        BrowserUtils.sleep(2);
+
+        hepsiburada_page.GirisYap.click();
+        BrowserUtils.sleep(2);
+
+        hepsiburada_page.GirisYapButton.click();
+        BrowserUtils.sleep(2);
+
+        hepsiburada_page.email.sendKeys("ahmet_eagle1903@hotmail.com");
+        BrowserUtils.sleep(2);
+
+        //hepsiburada_page.loginButton.click();
+        //BrowserUtils.sleep(2);
+
+
+    }
+
+}
